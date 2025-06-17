71b1df4959425234899da6c88fa21ec7e561c1fe
Victor <victorli1076@gmail.com>
2025-06-17 20:53:25 +0800

    tsai oversampling + label smoothing

--- tsai/training.py
+++ tsai/training.py
@@ -45,12 +45,12 @@ def process_record(record):
     return record_signals.T, record_age, record_sex, record_label
 
 def log(message):
-    with open('/users/vmli3/physionet-2025/tsai/log.txt', 'a') as f:
+    with open('/Users/victorli/Documents/GitHub/Physionet-2025/tsai/log_tcn.txt', 'a') as f:
         f.write(f"{message} \n")
         print(message)
 
-if os.path.exists('/users/vmli3/physionet-2025/tsai/log.txt'):
-    os.remove('/users/vmli3/physionet-2025/tsai/log.txt')
+if os.path.exists('/Users/victorli/Documents/GitHub/Physionet-2025/tsai/log_tcn.txt'):
+    os.remove('/Users/victorli/Documents/GitHub/Physionet-2025/tsai/log_tcn.txt')
 
 pst = pytz.timezone('US/Pacific')
 start_time = datetime.now(pst).strftime('%Y-%m-%d %H:%M:%S %Z')
@@ -60,7 +60,7 @@ log(f"Using device: {device}")
 
 my_setup()
 
-data_folder = '/scratch/vmli3/physionet-2025-data/preprocessed'
+data_folder = '/Users/victorli/Documents/GitHub/Physionet-2025-ECGFM-Submission/training_data'
 records = find_records(data_folder)
 random.shuffle(records)
 train_records, test_records = train_test_split(records, test_size=0.2, random_state=42)
@@ -76,20 +76,20 @@ sex = []
 labels = []
 
 
-batch_size = 16384
-inner_batch_size = 128
-num_workers = 8
-num_epochs = 30
+batch_size = 4
+inner_batch_size = 2
+num_workers = 1
+num_epochs = 3
 lr = 1e-3
 archs = [
-    (LSTM, {}),                # LSTM (Hochreiter, 1997)
-    (GRU, {}),                 # GRU (Cho, 2014)
-    (MLP, {}),                 # MLP - Multilayer Perceptron (Wang, 2016)
-    (FCN, {}),                 # FCN - Fully Convolutional Network (Wang, 2016)
-    (ResNet, {}),              # ResNet - Residual Network (Wang, 2016)
-    (LSTM_FCN, {}),            # LSTM-FCN (Karim, 2017)
-    (GRU_FCN, {}),             # GRU-FCN (Elsayed, 2018)
-    (mWDN, {'levels': 4}),     # mWDN - Multilevel wavelet decomposition network (Wang, 2018)
+    # (LSTM, {}),                # LSTM (Hochreiter, 1997)
+    # (GRU, {}),                 # GRU (Cho, 2014)
+    # (MLP, {}),                 # MLP - Multilayer Perceptron (Wang, 2016)
+    # (FCN, {}),                 # FCN - Fully Convolutional Network (Wang, 2016)
+    # (ResNet, {}),              # ResNet - Residual Network (Wang, 2016)
+    # (LSTM_FCN, {}),            # LSTM-FCN (Karim, 2017)
+    # (GRU_FCN, {}),             # GRU-FCN (Elsayed, 2018)
+    # (mWDN, {'levels': 4}),     # mWDN - Multilevel wavelet decomposition network (Wang, 2018)
     (TCN, {}),                 # TCN - Temporal Convolutional Network (Bai, 2018)
     # (MLSTM_FCN, {}),           # MLSTM-FCN - Multivariate LSTM-FCN (Karim, 2019)
     # (InceptionTime, {}),       # InceptionTime (Fawaz, 2019)
@@ -113,59 +113,77 @@ for i, (arch, k) in enumerate(archs):
     log(f"********************** {arch.__name__} **********************")
     for split in range(1):
         log(f"------------------- Fold {split} -------------------")
-        # log(f"Training {arch.__name__}")
-        # start = time.time()
-        # model = create_model(arch, c_in=12, c_out=2, seq_len=4096, **k)
-        # for epoch in range(num_epochs):
-        #     log(f"Epoch {epoch + 1}/{num_epochs} for {arch.__name__}")
-        #     for batch_index in range((num_train_records + batch_size - 1) // batch_size):
-        #         batch_start = time.time()
-        #         start_index = batch_index * batch_size
-        #         end_index = min((batch_index + 1) * batch_size, num_train_records)
-        #         records_batch = train_records[start_index:end_index]
+        log(f"Training {arch.__name__}")
+        start = time.time()
+        model = create_model(arch, c_in=12, c_out=2, seq_len=4096, **k)
+        # load_model(model=model, device=device, opt=None, file=f'/Users/victorli/Documents/GitHub/Physionet-2025/tsai/{arch.__name__}', with_opt=False)
+        model.train()
+        for epoch in range(num_epochs):
+            log(f"Epoch {epoch + 1}/{num_epochs} for {arch.__name__}")
+            for batch_index in range((num_train_records + batch_size - 1) // batch_size):
+                batch_start = time.time()
+                start_index = batch_index * batch_size
+                end_index = min((batch_index + 1) * batch_size, num_train_records)
+                records_batch = train_records[start_index:end_index]
 
-        #         ecg_signals = []
-        #         age = []
-        #         sex = []
-        #         labels = []
+                # ecg_signals = []
+                # age = []
+                # sex = []
+                # labels = []
 
-        #         with ThreadPoolExecutor() as executor:
-        #             results = list(executor.map(process_record, records_batch))
+                with ThreadPoolExecutor() as executor:
+                    results = list(executor.map(process_record, records_batch))
 
-        #         ecg_signals, age, sex, labels = zip(*results)
+                ecg_signals, age, sex, labels = zip(*results)
 
-        #         X = np.array(ecg_signals)
-        #         y = np.array(labels)
-        #         splits = (list(np.arange(0, len(X))), [])
-        #         train_dsets = TSDatasets(X, y, splits=splits, inplace=True)
-        #         train_dls  = TSDataLoaders.from_dsets(train_dsets.train, train_dsets.valid, bs=[inner_batch_size, 0], shuffle=True, num_workers=num_workers)
+                # oversampling positive samples
+                ecg_signals = list(ecg_signals)
+                age = list(age)
+                sex = list(sex)
+                labels = list(labels)
 
-        #         learn = Learner(train_dls, model, loss_func=FocalLoss()) #FBeta is basically F1
-        #         learn.fit_one_cycle(1, lr)
-        #         avg_loss = round(np.sum(np.array(learn.recorder.losses)) / len(learn.recorder.losses), 6)
-        #         batch_elapsed = time.time() - batch_start
-        #         log(f"Processing batch {batch_index + 1}/{(num_train_records + batch_size - 1) // batch_size}, start_index: {start_index}, end_index: {end_index-1}, avg_loss: {avg_loss}, time: {batch_elapsed} seconds")
+                for i in range(len(labels) - 1, -1, -1):
+                    if labels[i] == 1:
+                        for j in range(9):
+                            ecg_signals.append(ecg_signals[i])
+                            age.append(age[i])
+                            sex.append(sex[i])
+                            labels.append(labels[i])
+                            
+                X = np.array(ecg_signals)
+                y = np.array(labels)
 
-        #         del ecg_signals
-        #         del age
-        #         del sex
-        #         del labels
-        #         del X
-        #         del y
-        #         del splits
-        #         del train_dsets
-        #         del train_dls
-        #         del learn
-        #         gc.collect()
-        #         torch.cuda.empty_cache()
+                splits = (list(np.arange(0, len(X))), [])
+                train_dsets = TSDatasets(X, y, splits=splits, inplace=True)
+                train_dls  = TSDataLoaders.from_dsets(train_dsets.train, train_dsets.valid, bs=[inner_batch_size, 0], shuffle=True, num_workers=num_workers)
 
-        #     log('\n')
+                # label smoothing
+                learn = Learner(train_dls, model, loss_func=FocalLoss(smoothing=0.01)) #FBeta is basically F1
+                learn.fit_one_cycle(1, lr)
+                avg_loss = round(np.sum(np.array(learn.recorder.losses)) / len(learn.recorder.losses), 6)
+                batch_elapsed = time.time() - batch_start
+                log(f"Processing batch {batch_index + 1}/{(num_train_records + batch_size - 1) // batch_size}, start_index: {start_index}, end_index: {end_index-1}, avg_loss: {avg_loss}, time: {batch_elapsed} seconds")
 
-        #     if epoch == num_epochs - 1:
-        #         save_model(model=model, opt=None, file=f'/scratch/vmli3/physionet-2025-data/tsai-models/{arch.__name__}', with_opt=False)
+                del ecg_signals
+                del age
+                del sex
+                del labels
+                del X
+                del y
+                del splits
+                del train_dsets
+                del train_dls
+                del learn
+                gc.collect()
+                torch.cuda.empty_cache()
 
-        # elapsed = time.time() - start
-        # log(f"Training time for {arch.__name__}: {elapsed} seconds")
+            log('\n')
+
+            if epoch == num_epochs - 1:
+                save_model(model=model, opt=None, file=f'/Users/victorli/Documents/GitHub/Physionet-2025/tsai/{arch.__name__}', with_opt=False)
+
+        elapsed = time.time() - start
+        log(f"Training time for {arch.__name__}: {elapsed} seconds")
 
         log(f"Evaluating {arch.__name__}")
         start = time.time()
@@ -174,7 +192,7 @@ for i, (arch, k) in enumerate(archs):
         test_preds = np.zeros((num_test_records,))
 
         model = create_model(arch, c_in=12, c_out=2, seq_len=4096, **k)
-        load_model(model=model, device=device, opt=None, file=f'/scratch/vmli3/physionet-2025-data/tsai-models/{arch.__name__}', with_opt=False)
+        load_model(model=model, device=device, opt=None, file=f'/Users/victorli/Documents/GitHub/Physionet-2025/tsai/{arch.__name__}', with_opt=False)
         model.eval()
 
         for i in range(num_test_records):
